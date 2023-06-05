39fb650985712bae1df72f4736d5d5efc49585b4
Aslam <12aslam213@gmail.com>
2023-06-05 13:28:49 +0530

    changes made

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java world (modified)");  
+     System.out.println("Hello Java world, Hi Everyone");  
     }  
     
 }  
