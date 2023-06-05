523379cb46089f1d68b0c3c53f83a552e3cdc545
Aslam <12aslam213@gmail.com>
2023-06-05 10:54:06 +0530

    mo commit

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java world");  
+     System.out.println("Hello Java world (modified)");  
     }  
     
 }  
